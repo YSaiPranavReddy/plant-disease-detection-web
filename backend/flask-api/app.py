bbba2e10113594b2590f588d7a9eecad755cb9e4
Sai Pranav Reddy <ysaipranavreddy09@gmail.com>
2025-10-18 11:49:02 +0530

    Update CORS for production frontend

--- backend/flask-api/app.py
+++ backend/flask-api/app.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 CORS(app, resources={
     r"/*": {
         "origins": [
+            "https://bloom--plant-disease-detector.vercel.app/",
             "http://localhost:8080",
             "http://127.0.0.1:8080",
             "http://localhost:5500",
